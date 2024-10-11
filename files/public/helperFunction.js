a2526e0989484897af000eda035ae633853d722c
noellewolf <noelle@invosolutions.com>
2024-10-11 12:55:35 -0500

    Making an async GET request

--- files/public/helperFunction.js
+++ files/public/helperFunction.js
@@ -1,36 +1,48 @@
-// Manipulates responseField to render a formatted and appropriate message
+// Formats response to look presentable on webpage
 const renderResponse = (res) => {
-    // Displays either message depending on results
-    if(res.errors){
-      responseField.innerHTML = "<p>Sorry, couldn't format your URL.</p><p>Try again.</p>";
-    } else {  
-      responseField.innerHTML = `<p>Your shortened url is: </p><p> ${res.shortUrl} </p>`;
+    // Handles if res is falsey
+    if(!res){
+      console.log(res.status);
     }
+    // In case res comes back as a blank array
+    if(!res.length){
+      responseField.innerHTML = "<p>Try again!</p><p>There were no suggestions found!</p>";
+      return;
+    }
+  
+    // Creates an empty array to contain the HTML strings
+    let wordList = [];
+    // Loops through the response and caps off at 10
+    for(let i = 0; i < Math.min(res.length, 10); i++){
+      // creating a list of words
+      wordList.push(`<li>${res[i].word}</li>`);
+    }
+    // Joins the array of HTML strings into one string
+    wordList = wordList.join("");
+  
+    // Manipulates responseField to render the modified response
+    responseField.innerHTML = `<p>You might be interested in:</p><ol>${wordList}</ol>`;
+    return
   }
   
-  // Manipulates responseField to render an unformatted response
+  // Renders response before it is modified
   const renderRawResponse = (res) => {
-    // Displays either message depending on results
-    if(res.errors){  
-      responseField.innerHTML = "<p>Sorry, couldn't format your URL.</p><p>Try again.</p>";
-    } else {
-      // Adds line breaks for JSON
-      let structuredRes = JSON.stringify(res).replace(/,/g, ", \n");
-      structuredRes = `<pre>${structuredRes}</pre>`;
-      responseField.innerHTML = `${structuredRes}`;
-    }
+    // Takes the first 10 words from res
+    let trimmedResponse = res.slice(0, 10);
+    // Manipulates responseField to render the unformatted response
+    responseField.innerHTML = `<text>${JSON.stringify(trimmedResponse)}</text>`;
   }
   
   // Renders the JSON that was returned when the Promise from fetch resolves.
-  const renderJsonResponse = (response) => {
+  const renderJsonResponse = (res) => {
     // Creates an empty object to store the JSON in key-value pairs
-    let rawJson = {}
-    for(let key in response){
-      rawJson[key] = response[key]
+    let rawJson = {};
+    for(let key in res){
+      rawJson[key] = res[key];
     }
     // Converts JSON into a string and adding line breaks to make it easier to read
-    rawJson = JSON.stringify(rawJson).replace(/,/g, ", \n")
+    rawJson = JSON.stringify(rawJson).replace(/,/g, ", \n");
     // Manipulates responseField to show the returned JSON.
-    responseField.innerHTML = `<pre>${rawJson}</pre>`
+    responseField.innerHTML = `<pre>${rawJson}</pre>`;
   }
   
--- files/public/main.js
+++ files/public/main.js
@@ -1,43 +1,35 @@
 // Information to reach API
-const apiKey = '<Your API Key>';
-const url = 'https://api.rebrandly.com/v1/links';
+const url = 'https://api.datamuse.com/words?';
+const queryParams = 'rel_jja=';
 
-// Some page elements
+// Selecting page elements
 const inputField = document.querySelector('#input');
-const shortenButton = document.querySelector('#shorten');
+const submit = document.querySelector('#submit');
 const responseField = document.querySelector('#responseField');
 
-// Asynchronous functions
-const shortenUrl = () => {
-  const urlToShorten = inputField.value;
-  const data = JSON.stringify({destination: urlToShorten});
-  
-	fetch(url, {
-    method: 'POST',
-    headers: {
-      'Content-type': 'application/json',
-      'apikey': apiKey
-    },
-    body: data
-  }).then(response => {
-    if (response.ok) {
-      return response.json();
+// Asynchronous function
+// Code goes here
+const getSuggestions = async () => {
+  const wordQuery = inputField.value;
+  const endpoint = `${url}${queryParams}${wordQuery}`;
+  try {
+    const response = await fetch(endpoint, {cache: 'no-cache'});
+    if(response.ok){
+      const jsonResponse = await response.json();
+      renderResponse(jsonResponse);
     }
-    throw new Error('Request failed!');
-  }, networkError => {
-    console.log(networkError.message);
-  }).then(jsonResponse => {
-    renderResponse(jsonResponse);
-  })
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-// Clear page and call Asynchronous functions
-const displayShortUrl = (event) => {
+// Clear previous results and display results to webpage
+const displaySuggestions = (event) => {
   event.preventDefault();
   while(responseField.firstChild){
     responseField.removeChild(responseField.firstChild);
   }
-  shortenUrl();
+  getSuggestions();
 }
 
-shortenButton.addEventListener('click', displayShortUrl);
+submit.addEventListener('click', displaySuggestions);
